68040dd2fb7685fe2ccd9b153695b9c590bb74b8
Jennifer White <uscgjen@gmail.com>
2020-11-24 13:45:32 -0500

    Fizz Buzz Solution

---
+++ FizzBuzz/FizzBuzzHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace FizzBuzz
+{
+    public class FizzBuzzHelper
+    {
+        public FizzBuzzHelper()
+        {
+        }
+
+        public string ReturnString(int value)
+        {
+            if (value % 3 == 0 && value % 5 == 0)
+                return "FizzBuzz";
+            else if (value % 3 == 0)
+                return "Fizz";
+            else if (value % 5 == 0)
+                return "Buzz";
+            else
+                return string.Empty;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzHelper helper = new FizzBuzzHelper();
+            string result = String.Empty;
+            for (int i = 0; i < 100; i++)
+            {
+                result = helper.ReturnString(i);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    Console.WriteLine($"{result}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
